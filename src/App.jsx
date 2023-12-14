e5e575f86d958633b2c6c90d9832b0c056c8f716
Dario Rodrigo Torrejon <dartorrejon432@outlook.com>
2023-12-13 21:52:16 -0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./assets/components/Header/Header"
+
+function App() {
+  
+
+  return (
+    <>
+        <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Text } from '@chakra-ui/react'
+const Header = () => {
+    return (
+        <Text
+        as='h1'
+        bg="red.300"
+        lineHeight='2em'
+        p='0 20px'>Hola Mundo</Text>
+    )
+}
+
+export default Header
